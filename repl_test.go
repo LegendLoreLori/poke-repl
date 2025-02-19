21e42db63b2bcfac62be74eef5c8d17cf8036d09
Lori <lorenzomandolfino@gmail.com>
2025-02-19 13:01:05 +0800

    add tests for cache add get and reaploop

--- repl_test.go
+++ repl_test.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"testing"
+	"time"
 )
+
+var pass int
+var fail int
 
 func TestCleanInput(t *testing.T) {
 	cases := []struct {
@@ -35,8 +39,6 @@ func TestCleanInput(t *testing.T) {
 			expected: []string{"TODO: implement err handling"},
 		},
 	}
-	pass := 0
-	fail := 0
 	for _, c := range cases {
 		actual := cleanInput(c.input)
 		if len(actual) != len(c.expected) {
@@ -53,7 +55,68 @@ func TestCleanInput(t *testing.T) {
 			}
 		}
 		pass++
-		fmt.Printf("-----------------\ninput: '%v'\nexpected: %v\nactual: %v\n", c.input, c.expected, actual)
 	}
+}
+
+func TestCacheAddGet(t *testing.T) {
+	cases := []struct {
+		key string
+		val []byte
+	}{
+		{
+			key: "https://example.com/",
+			val: []byte("test data"),
+		},
+		{
+			key: "https://example.com/?query=test&query2=test2",
+			val: []byte("test data from query"),
+		},
+	}
+	const interval = 5 * time.Millisecond
+	for _, c := range cases {
+		testCache := NewCache(interval)
+
+		testCache.Add(c.key, c.val)
+		val, ok := testCache.Get(c.key)
+		if !ok {
+			fail++
+			t.Errorf("expected to find key")
+			return
+		}
+		if string(val) != string(c.val) {
+			fail++
+			t.Error("expected to find value")
+			return
+		}
+		pass++
+	}
+}
+
+func TestReapLoop(t *testing.T) {
+	const baseTime = 5 * time.Millisecond
+	const waitTime = baseTime + 5*time.Millisecond
+	cache := NewCache(baseTime)
+	cache.Add("https://example.com", []byte("testdata"))
+
+	_, ok := cache.Get("https://example.com")
+
+	if !ok {
+		fail++
+		t.Errorf("expected to find key")
+		return
+	}
+
+	time.Sleep(waitTime) // maybe i can wrap this in a go func later? sleeping the whole test suite seems cringe
+
+	_, ok = cache.Get("https://example.com")
+	if ok {
+		fail++
+		t.Errorf("expected to not find key")
+		return
+	}
+	pass++
+}
+
+func TestPrintPassFail(t *testing.T) {
 	fmt.Printf("-----------------\npass: %v, fail: %v\n", pass, fail)
 }
